e8c9b0ce9cfa32ccd3018d7c6e053cd6326bd98b
David Whittemore <github@xpollen8.com>
2021-11-13 08:43:50 -0800

    dw: added flac/mp4

--- components/Uploader.js
+++ components/Uploader.js
@@ -106,7 +106,7 @@ const AssetUploader = ({ who = 'MISSING', id = 'MISSING', value = '', setValue }
 				}
 				<input onChange={handleChange}
 					ref={(ref) => { setUploadInput(ref); }}
-					type="file" accept="image/jpeg, image/png, video/mp4, audio/mp3" hidden />
+					type="file" accept="image/jpeg, image/png, video/mp4, audio/mp3, audio/flac, audio/x-aac" hidden />
 				{(newFile && !uploading) &&
 					<button onClick={handleUpload}>Upload it!</button>
 				}
