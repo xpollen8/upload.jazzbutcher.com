72de93935d381c188dcdcb7d7b98d9a7dcedeb58
David Whittemore <github@xpollen8.com>
2022-06-24 19:06:57 -0700

    dw: add title

--- components/Layout.js
+++ components/Layout.js
@@ -3,6 +3,7 @@ import Footer from './Footer'
 
 const Layout = ({ title, children }) => (
 	<>
+	<h1 style={{ textAlign: 'center' }}>Jazz Butcher file uploader</h1>
 	<Header title={title} />
 		{children}
 	<Footer/>
