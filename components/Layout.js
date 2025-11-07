4dee5364a234a00867b2197337df38f61268569a
xpollen8 <github@xpollen8.com>
2025-11-06 22:19:54 -0800

    dw: mods

--- components/Layout.js
+++ components/Layout.js
@@ -5,8 +5,10 @@ import Footer from './Footer'
 const Layout = ({ title, children }) => (
 	<>
 		<center>
+			<p/>
 			<Image src='/images/scandal_40.png' width={200} height={40} alt='logo' />
-			<h2>File Uploader</h2>
+			<p/>
+			<b>File Uploader</b>
 		</center>
 		<Header title={title} />
 			{children}
