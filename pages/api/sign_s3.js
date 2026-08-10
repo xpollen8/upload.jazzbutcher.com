6409cf5c01276b6d97e22f9b0906e63fcaa35627
David Whittemore <github@xpollen8.com>
2026-08-09 19:42:23 -0700

    dw: rm ContentType

--- pages/api/sign_s3.js
+++ pages/api/sign_s3.js
@@ -1,10 +1,10 @@
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { S3RequestPresigner, getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { S3Client, GetSignedObject, PutObjectCommand } from "@aws-sdk/client-s3";
 
 const handler = async (req, res) => {
 	try {
 		const fileName = req.body.fileName;
-		const fileType = req.body.fileType;
+		//const fileType = req.body.fileType;
 		const s3Client = new S3Client({
 			region: process.env.JBC_AWS_REGION,
 			accessKeyId: process.env.JBC_AWS_ACCESSKEYID,
@@ -15,7 +15,7 @@ const handler = async (req, res) => {
 			{
 				Bucket: process.env.JBC_AWS_BUCKET,
 				Key: fileName,
-				ContentType: fileType,
+				//ContentType: fileType,
 			}
 		);
 		const signedURL = await getSignedUrl(s3Client, command, { expiresIn: 3600 });
