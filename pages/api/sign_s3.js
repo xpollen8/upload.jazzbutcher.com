941b62a28af079e4c7942c7f52cecd6a5691977d
David Whittemore <github@xpollen8.com>
2026-08-10 00:33:04 -0700

    dw: pickup from default

--- pages/api/sign_s3.js
+++ pages/api/sign_s3.js
@@ -3,10 +3,9 @@ import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
 const handler = async (req, res) => {
 	const key = req.body.fileName;
-	const region = process.env.JBC_AWS_REGION;
 	const bucket = process.env.JBC_AWS_BUCKET;
 
-	const client = new S3Client({ region });
+	const client = new S3Client();
 	const command = new PutObjectCommand({ Bucket: bucket, Key: key });
 	const signedRequest = await getSignedUrl(client, command, { expiresIn: 3600 });
 	console.log("API", { success: true, returnedData: { url: key, signedRequest }});
