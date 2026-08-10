cae71c72cbbaf26e3b0b66a40ac9358bbe17d2a5
David Whittemore <github@xpollen8.com>
2026-08-10 00:03:54 -0700

    dw: force credentials from env

--- pages/api/sign_s3.js
+++ pages/api/sign_s3.js
@@ -1,14 +1,15 @@
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
-const region = process.env.JBC_AWS_REGION;
-const bucket = process.env.JBC_AWS_BUCKET;
-const accessKeyId = process.env.JBC_AWS_ACCESSKEYID;
-const secretAccessKey = process.env.JBC_AWS_SECRETACCESSKEY;
-
 const handler = async (req, res) => {
 	const key = req.body.fileName;
-	const client = new S3Client({ region, accessKeyId, secretAccessKey });
+	const region = process.env.JBC_AWS_REGION;
+	const bucket = process.env.JBC_AWS_BUCKET;
+
+	const client = new S3Client({ region, credentials: {
+	    secretAccessKey: process.env.JBC_AWS_ACCESSKEYID,
+			accessKeyId: process.env.JBC_AWS_SECRETACCESSKEY,
+	}});
 	const command = new PutObjectCommand({ Bucket: bucket, Key: key });
 	const signedRequest = await getSignedUrl(client, command, { expiresIn: 3600 });
 	console.log("API", { success: true, returnedData: { url: key, signedRequest }});
